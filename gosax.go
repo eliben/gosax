5046e14e49becbd80f718b9918b80b26d8bc1e96
Eli Bendersky <eliben@gmail.com>
2019-07-06 06:22:44 -0700

    Init xmlInitParser once

--- gosax.go
+++ gosax.go
@@ -1,1 +1,21 @@
 package gosax
+
+import "sync"
+
+/*
+#cgo pkg-config: libxml-2.0
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/parserInternals.h>
+*/
+import "C"
+
+// Used to ensure that xmlInitParser is only called once.
+var initOnce sync.Once
+
+func init() {
+	initOnce.Do(func() {
+		C.xmlInitParser()
+	})
+}
--- gosax_test.go
+++ gosax_test.go
@@ -1,1 +1,7 @@
 package gosax
+
+import "testing"
+
+func TestInit(*testing.T) {
+	// Just testing that nothing crashed during init
+}
